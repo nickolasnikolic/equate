a04c293bd06278bd68102ff6f4c24b46ff838f85
Nickolas Nikolic <nickolasnikolic@gmail.com>
2015-10-22 02:26:29 -0500

    equation listing

--- routes/equations.js
+++ routes/equations.js
@@ -6,6 +6,7 @@ var Equation = require('../models/Equation');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Equation.find({},function(err, equations){
+    console.log(equations);
     res.render('equations', { title: 'boo', equations: equations });
   });
 });
