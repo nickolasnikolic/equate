341d683b2b0b3a09ffa1fda4def7e754206ba500
Nickolas Nikolic <subclass@gmail.com>
2015-10-22 13:57:56 -0500

    css

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -1,17 +1,22 @@
 blindApp.controller('IndexController', ['$scope', '$state', 'globals', function($scope, $state, globals) {}])
 
-blindApp.controller('HomeController', ['$scope', '$state', '$http', 'globals', function($scope, $state, $http, globals) {
+blindApp.controller('HomeController', ['$scope', '$state', '$http', '$sce', 'globals', function($scope, $state, $http, $sce, globals) {
   document.title = 'blindworm - home'; //set the page title
 
   $http.get('/api/equations')
       .then(function(response){
         $scope.equations = response.data;
-        MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
       });
 
   $scope.submitEq = function( obj ){
     $http.post('/api/equation', obj);
   };
 
-}])
+  $scope.mathMl = function(returnTrusted, index){
+      var blindlyTrusting = returnTrusted[index];
+      var asciiMathParser = new AsciiMathParser(document);
+      blindlyTrusting = asciiMathParser.parseAsciiMathInput(blindlyTrusting);
+      return $sce.trustAsHtml(blindlyTrusting);
+  };
 
+}])
